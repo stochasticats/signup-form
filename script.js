4db1048ce071eb7eb3c7fb1ce277ad63ba29c7c8
Jamie <nordio.jamie@gmail.com>
2022-12-01 10:15:13 -0800

    add javascript to check if passwords match, throws error message if do not match

---
+++ script.js
@@ -0,0 +1,14 @@
+// setup variables
+const pass = document.getElementById("pass");
+const passC = document.getElementById("passC");
+
+const passCError = document.getElementById("passwordC-error");
+
+// event listeners
+passC.addEventListener("input", e => {
+    if (passC.value !== pass.value) {
+        passCError.textContent = "Passwords do not match";
+    } else {
+        passCError.textContent = "";
+    }
+});
