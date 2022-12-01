3abe99cef150793602ea0b62028bcf80a5932fe9
Jamie <nordio.jamie@gmail.com>
2022-12-01 10:22:59 -0800

    add setCustomValidity to throw invalid field if passwords do not match, valid if they match

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // setup variables
 const pass = document.getElementById("pass");
 const passC = document.getElementById("passC");
-
 const passCError = document.getElementById("passwordC-error");
+const form = document.getElementById("signup-form");
 
 // event listeners
 passC.addEventListener("input", e => {
     if (passC.value !== pass.value) {
         passCError.textContent = "Passwords do not match";
+        passC.setCustomValidity("Invalid field.");
     } else {
         passCError.textContent = "";
+        passC.setCustomValidity("");
     }
 });
